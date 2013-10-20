57f405203bb517216f954030b796ba6104b50ffc
Ben Chao <bchao@cs.washington.edu>
2013-10-20 16:58:11 -0400

    Fixing displays

--- js/app.js
+++ js/app.js
@@ -8,13 +8,44 @@ if (!String.prototype.trim) {
 // define our list class
 function List () {
 
+  var showed_warning = false; 
+
+  this.show_input_warning = function () {
+    $('#add-item').val('').attr('placeholder', 'Please enter an item that is NOT empty...');
+    showed_warning = true; 
+  }
+
+  this.reset_add = function () {
+
+    var $add_item = $('#add-item');
+
+    if ( showed_warning ) {
+      showed_warning = false; 
+      $add_item.attr('placeholder', 'Enter an item...');
+    }
+
+    $add_item.val('');
+  }
+
+  this.check_empty = function () {
+    if ( $('.item').length > 0 ) {
+      $('#empty-list-msg').hide();
+      $('#buttons').show();
+    } else {
+      $('#empty-list-msg').fadeIn(150);
+      $('#buttons').hide();
+    }
+  }
+
   this.add_item = function ( item ) {
     var new_item = '<li class="item"><input type="checkbox"><label>' + item + '</label></li>'
-    $('#list').prepend(new_item);
+    $("#list").prepend(new_item);
+    this.check_empty();
   }
 
   this.clear_checked = function () {
     $(".checked").remove();
+    this.check_empty();
   }
 
   this.check_all = function () {
@@ -50,18 +81,21 @@ $(document).ready(function(){
     }
   });
 
-  // listen to events 
   $('form').keypress( function(e) {
 
     // listen for enter
     if (e.keyCode === 13) {
 
+      console.log("got osmething");
+
       var item = new String($('#add-item').val());
-      item = item.trim();
+      item = item.trim(); /* remove white space */
 
       if ( item ) {
         this_list.add_item(item);
-        $('#add-item').val('');
+        this_list.reset_add();
+      } else {
+        this_list.show_input_warning();
       }
 
       // prevent form reloading
